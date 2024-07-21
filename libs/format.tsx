7edeb28f561f6d61d62c190772f623e8cad338e9
Rodrigo <rodrigoinfante766@gmail.com>
2024-07-21 10:40:38 -0400

    function q formatea la respuesta de la apiTest a sido actualizada para formatear el campo match

--- libs/format.tsx
+++ libs/format.tsx
@@ -1,26 +1,43 @@
 import { TestWithSelect } from "@/types/contextTypes"
-import { TestList } from "@/types/shemaTest"
+import { TestList,Match } from "@/types/shemaTest"
 
 export function addPropsClientUi(tests: TestList): TestWithSelect[]{
     return tests.map((test)=>{
         return {
-            ...test, 
+            about:test.about, 
+            
             choises: test.choises.map((choise)=>{
                 return {
                     ...choise,
                     selected: false
                 }
             }),
+            
             complete: test.complete.map((complete)=>{
                 return{
                     sentence: complete,
                     response: ""
                 }
             }),
+            
             question:{
                 sentence: test.question,
                 response: ""
-            }
+            },
+            
+            match: test.match.map((item)=>{
+                return {
+                    columnA:{
+                        content: item.columnA,
+                        selected:false
+                    },
+                    columnB:{
+                        content: item.columnB,
+                        selected:false
+                    },
+                    correct: null 
+                } 
+            })
         }
     }) 
 }
