096e9e08f9481a8f418d48da309eb40205af5b63
Rodrigo <rodrigoinfante766@gmail.com>
2024-07-21 13:43:54 -0400

    TestBody About actualizado par q se adecue a cada tipo de Test

--- components/Test/Body/TestBodyAbout.tsx
+++ components/Test/Body/TestBodyAbout.tsx
@@ -1,5 +1,5 @@
 export function TestBodyAbout({children}:{children:string}) {
     return(
-        <p className="text-lg font-medium">Sobre ( {children} ) seleccione correctamente las expresiones verdaderas :</p>
+        <p className="text-lg font-medium ">Sobre ( <span className="text-red-400">{children}</span>  ) :</p>
     )
 }
